aa5f34d8feb91b955f8859c5bc516bced59f6db0
Iliasabs <205830257+iliasabs@users.noreply.github.com>
2025-12-25 15:11:11 +0100

    syntax issue fixed

--- next.config.ts
+++ next.config.ts
@@ -236,11 +236,6 @@ export default withSentryConfig(nextConfig, {
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
   tunnelRoute: "/monitoring",
 
   // Webpack-based configuration replacing deprecated options
@@ -249,10 +244,6 @@ export default withSentryConfig(nextConfig, {
     treeshake: {
       removeDebugLogging: true,
     },
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
     automaticVercelMonitors: true,
   },
-} as unknown as NextConfig);
+} as any);
