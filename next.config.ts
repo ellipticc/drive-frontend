d8fc54e9f046b74c46d5c8a714462bcae6dc7b14
Iliasabs <205830257+iliasabs@users.noreply.github.com>
2026-02-28 16:26:06 +0100

    update: transition to SSR build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   trailingSlash: false,
   productionBrowserSourceMaps: true,
   images: {
